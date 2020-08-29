d74c734e5a4d5ab888f48e8e22a7f15af25348c6
Hamza <hamzabitup@gmail.com>
2020-08-29 20:40:21 +0100

    Add: queries and mutations to post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,54 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { IContext } from 'src/types';
+import { NullValueNode } from 'graphql';
 
 @Resolver()
 export class PostResolver {
+	// get posts
 	@Query(() => [Post])
 	posts(@Ctx() { em }: IContext): Promise<Post[]> {
 		return em.find(Post, {});
+	}
+
+	// get post
+	@Query(() => Post, { nullable: true })
+	post(
+		@Arg('id', () => Int) id: number,
+		@Ctx() { em }: IContext
+	): Promise<Post | null> {
+		return em.findOne(Post, { id });
+	}
+
+	// create post
+	@Mutation(() => Post)
+	async createPost(
+		@Arg('title', () => String) title: string,
+		@Ctx() { em }: IContext
+	): Promise<Post> {
+		const post = em.create(Post, {
+			title,
+		});
+		await em.persistAndFlush(post);
+
+		return post;
+	}
+
+	// update post
+	@Mutation(() => Post)
+	async updatePost(
+		@Arg('id') id: number,
+		@Arg('title', () => String, { nullable: true }) title: string,
+		@Ctx() { em }: IContext
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { id });
+		if (!post) {
+			return null;
+		}
+		if (typeof title !== 'undefined') {
+			post.title = title;
+			em.persistAndFlush(post);
+		}
+		return post;
 	}
 }
